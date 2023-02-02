06df88adeaed480beefdb28c53b76ab46b7e17ac
David Fernando Castro Valerio <davidcastro.8.4.1991@gmail.com>
2023-02-01 20:31:16 -0600

    Red to antiquewhite

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -20,7 +20,7 @@ void MainComponent::paint (juce::Graphics& g)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
     g.setFont (juce::Font (48.0f));
-    g.setColour (juce::Colours::red);
+    g.setColour (juce::Colours::antiquewhite);
     g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
