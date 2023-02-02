70b2204a23eeec935b333f0dd244be622f73d77e
David Fernando Castro Valerio <davidcastro.8.4.1991@gmail.com>
2023-02-01 20:18:21 -0600

    Hello World Text Again

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -21,7 +21,7 @@ void MainComponent::paint (juce::Graphics& g)
 
     g.setFont (juce::Font (48.0f));
     g.setColour (juce::Colours::red);
-    g.drawText ("Hello Bruh!", getLocalBounds(), juce::Justification::centred, true);
+    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
